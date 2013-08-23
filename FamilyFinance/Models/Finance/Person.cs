62356342f1f07475fbe0b232c1d2dfc648847b10
Damo <damian.stanger@gmail.com>
2013-08-23 13:23:07 +0100

    added models

---
+++ FamilyFinance/Models/Finance/Person.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FamilyFinance.Models.Finance
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+    }
+}
---
+++ FamilyFinance/Models/Finance/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FamilyFinance.Models.Finance
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public virtual int AccountId { get; set; }
+        public virtual Account Account { get; set; }
+        public virtual int OwnerId { get; set; }
+        public virtual Person Owner { get; set; }
+    }
+}
