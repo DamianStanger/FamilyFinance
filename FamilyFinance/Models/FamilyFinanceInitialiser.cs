5230b2dd9e85e1cc2cd670207d5d6fe1d98f6344
Damo <damian.stanger@gmail.com>
2013-08-23 18:35:37 +0100

    Added more seed data, remove the DB

--- FamilyFinance/Models/FamilyFinanceInitialiser.cs
+++ FamilyFinance/Models/FamilyFinanceInitialiser.cs
@@ -13,6 +13,31 @@ namespace FamilyFinance.Models
             context.People.Add(new Person{Name = "Damo"});
             context.People.Add(new Person{Name = "Kat"});
             context.People.Add(new Person{Name = "Cara"});
+
+            context.Accounts.Add(new Account {Bank = "Barclays", Name = "Damo Current"});
+            context.Accounts.Add(new Account {Bank = "Barclays", Name = "Joint Current"});
+            context.Accounts.Add(new Account {Bank = "Barclays", Name = "Damo Savings"});
+            context.Accounts.Add(new Account {Bank = "Barclays", Name = "Cara Savings"});
+            context.Accounts.Add(new Account {Bank = "Lloyds", Name = "Damo Curent 2"});
+            context.Accounts.Add(new Account {Bank = "Lloyds", Name = "Damo ISA"});
+            context.Accounts.Add(new Account {Bank = "Barclaycard", Name = "Damo Barclaycard"});
+            context.Accounts.Add(new Account {Bank = "Barclaycard", Name = "Joint Barclaycard"});
+
+            context.Categories.Add(new Category {Name = "Travel"});
+            context.Categories.Add(new Category {Name = "Lunch"});
+            context.Categories.Add(new Category {Name = "Car"});
+            context.Categories.Add(new Category {Name = "Cara"});
+            context.Categories.Add(new Category {Name = "Shopping"});
+            context.Categories.Add(new Category {Name = "Food"});
+            context.Categories.Add(new Category {Name = "Ingenie"});
+            context.Categories.Add(new Category {Name = "GreenSphere"});
+            context.Categories.Add(new Category {Name = "Gas"});
+            context.Categories.Add(new Category {Name = "Elec"});
+            context.Categories.Add(new Category {Name = "Water"});
+            context.Categories.Add(new Category {Name = "CouncilTax"});
+            context.Categories.Add(new Category {Name = "Insurance"});
+            context.Categories.Add(new Category {Name = "Phone"});
+            
             context.SaveChanges();
         }
 
