60e4451e053a405572d16540df56acff8b9e1171
Damo <damian.stanger@gmail.com>
2013-09-07 21:51:59 +0100

    Added the account activity controller to view all transactions in order

---
+++ FamilyFinance/Controllers/AccountActivityController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FamilyFinance.Models.Repository;
+using FamilyFinance.Models.ViewModel;
+
+namespace FamilyFinance.Controllers
+{
+    public class AccountActivityController : Controller
+    {
+
+        private readonly ITransactionRepository transactionRepository;
+
+		// If you are using Dependency Injection, you can delete the following constructor
+        public AccountActivityController() : this(new TransactionRepository())
+        {
+        }
+
+        public AccountActivityController(TransactionRepository transactionRepository)
+        {
+            this.transactionRepository = transactionRepository;
+        }
+
+        //
+        // GET: /AccountActivity/
+
+        public ActionResult Index()
+        {
+            var transactions = transactionRepository.All.OrderBy(x => x.Date);
+            var viewModel = new List<AccountActivityViewModel>();
+            double runningTotal = 0d;
+            foreach (var transaction in transactions)
+            {
+                runningTotal += transaction.Amount;
+                var activity = new AccountActivityViewModel(transaction, runningTotal);
+                viewModel.Add(activity);
+            }
+            viewModel.Reverse();
+            return View(viewModel);
+        }
+    }
+}
---
+++ FamilyFinance/Models/ViewModel/AccountActivityViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using FamilyFinance.Models.Domain;
+
+namespace FamilyFinance.Models.ViewModel
+{
+    public class AccountActivityViewModel
+    {
+        public DateTime Date { get; set; }
+        public string Category { get; set; }
+        public string Account { get; set; }
+        public double Amount { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public double RunningTotal { get; set; }
+
+        public AccountActivityViewModel(Transaction transaction, double runningTotal)
+        {
+            Date = transaction.Date;
+            Category = transaction.Category.Name;
+            Account = transaction.Account.Name;
+            Amount = transaction.Amount;
+            Description = transaction.Description;
+            Name = transaction.Account.Name;
+            RunningTotal = runningTotal;
+        }
+    }
+}
